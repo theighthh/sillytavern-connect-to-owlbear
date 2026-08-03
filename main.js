63ea64a272567555e476e9dac200f9d49dd2e0f9
theighthh <15140514910@163.com>
2026-08-03 15:20:48 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import OBR from "https://cdn.jsdelivr.net/npm/@owlbear-rodeo/sdk@0.9.1/dist/index.mjs";
+import OBR from 'https://cdn.jsdelivr.net/npm/@owlbear-rodeo/sdk@3.1.0/+esm';
 
 // WebSocket 服务器地址（根据实际修改）
 const WS_URL = "ws://localhost:8080";
